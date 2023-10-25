084d943c17964cfad68cca20b954ca8ee2f411c7
NikolajRask <nikolaj.rask.andersen@gmail.com>
2023-10-25 17:09:04 +0200

    Added skills with cool effects

--- scripts/play.js
+++ scripts/play.js
@@ -31,3 +31,15 @@ document.addEventListener('mousemove', (e) => {
     }
     
 })
+
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
